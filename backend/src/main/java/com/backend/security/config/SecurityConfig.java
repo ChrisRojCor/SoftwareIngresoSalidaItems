8d3213da1e5cada1d58339f125f55955da3bdc3a
Cris <chrisrojcor@gmail.com>
2026-05-19 22:35:35 -0500

    feat: add JWT authentication filter; implement JwtAuthenticationFilter to validate tokens and integrate it into SecurityConfig

--- backend/src/main/java/com/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.backend.security.config;
 
+import com.backend.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,9 +11,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -19,6 +25,11 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
             .requestMatchers("/auth/**").permitAll()
             .anyRequest().authenticated()
+        );
+
+        http.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
         );
 
         return http.build();
