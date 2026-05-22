8c7c25a8883a6811738050f4608f7374b615846c
Cris <chrisrojcor@gmail.com>
2026-05-21 19:16:51 -0500

    feat: add validation annotations to LoginRequestDto and implement global exception handler for validation errors

---
+++ backend/src/main/java/com/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.backend.exception;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleValidationExceptions(MethodArgumentNotValidException ex){
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 400);
+        response.put("error", "Validation Error");
+        response.put("messages", errors);
+
+        return response;
+        
+    }
+
+
+}
