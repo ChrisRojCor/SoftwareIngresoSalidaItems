58face8730b637b14201fba8d12e69dd2c8c1a5a
Cris <chrisrojcor@gmail.com>
2026-05-21 19:37:41 -0500

    feat: enhance user registration by adding password encryption in UserService

--- backend/src/main/java/com/backend/service/UserService.java
+++ backend/src/main/java/com/backend/service/UserService.java
@@ -3,7 +3,9 @@ package com.backend.service;
 import com.backend.mapper.UserMapper;
 import com.backend.model.dto.UserRequestDto;
 import com.backend.model.dto.UserResponseDto;
+import com.backend.model.entity.User;
 import com.backend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,16 +15,18 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserResponseDto saveUser(UserRequestDto userRequestDto) {
-        return userMapper.toResponseDTO(
-                userRepository.save(
-                        userMapper.toEntity(userRequestDto)));
+    public UserResponseDto saveUser(UserRequestDto dto) {
+        User user = userMapper.toEntity(dto);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        return userMapper.toResponseDTO(userRepository.save(user));
     }
 
     public List<UserResponseDto> getAllUsers() {
