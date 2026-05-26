48604446e9979ef39d276994818b1643b794c557
Cris <chrisrojcor@gmail.com>
2026-05-26 14:49:38 -0500

    feat: add CORS configuration to allow requests from localhost and update security filter chain

--- backend/src/main/java/com/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/security/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http.csrf(csrf -> csrf.disable())
+        http.cors().csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .exceptionHandling(ex -> ex.accessDeniedHandler(customAccessDeniedHandler))
